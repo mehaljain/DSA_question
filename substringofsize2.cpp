addc5706599d6df935d6e65609b3b4ecfb4e236e
mehal jain <madhurjain2480@gmail.com>
2024-08-28 23:45:15 +0530

    dsa_practise

--- substringofsize2.cpp
+++ substringofsize2.cpp
@@ -4,15 +4,34 @@
 
 using namespace std;
 
+// Function to extract a substring from a given string
+string extractSubstring(const string& str, int start, int length) {
+    string substring;
+    for (int i = start; i < start + length; i++) {
+        substring += str[i];
+    }
+    return substring;
+}
+
+// Function to convert a string to an integer manually
+int stringToInt(const string& str) {
+    int num = 0;
+    for (int i = 0; i < str.length(); i++) {
+        num = num * 10 + (str[i] - '0');
+    }
+    return num;
+}
+
 int findMaxSum(string s) {
     int maxSum = 0;
 
     for (int i = 1; i < 4; i++) {
-        string part1 = s.substr(0, i);
-        string part2 = s.substr(i);
+        // Extract part1 and part2 manually
+        string part1 = extractSubstring(s, 0, i);
+        string part2 = extractSubstring(s, i, s.length() - i);
 
-        int num1 = stoi(part1);
-        int num2 = stoi(part2);
+        int num1 = stringToInt(part1);
+        int num2 = stringToInt(part2);
 
         int currentSum = num1 + num2;
 
@@ -24,8 +43,8 @@ int findMaxSum(string s) {
 
 int main() {
 
-    string s ;
-    cin>>s;
+    string s;
+    cin >> s;
     cout << "Maximum sum from the number \"" << s << "\" is: " << findMaxSum(s) << endl;
     return 0;
 }
